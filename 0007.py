ed01d30b57d336895daaa9151484ccdc15994b70
jeff lin <jeff1050930@gmail.com>
2021-07-07 14:13:18 +0800

    new file

--- 0007.py
+++ 0007.py
@@ -1,12 +1,14 @@
 password='a123456'
 x=3
-while True:
+while x>0:
+	x=x-1
 	pwd=input('請輸入密碼')
 	if pwd==password:
 		print('輸入成功')
 		break
 	else:
-		x=x-1
-		print('輸入失敗還有',x,'機會')
-		if x==0:
-			break
+		print('輸入失敗')
+		if x>0:
+			print('輸入失敗還有',x,'次機會')
+		else:
+			print('帳號被鎖住了!')
